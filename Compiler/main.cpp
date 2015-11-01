686b7531ba67452c1d281cae7f9728d21eb371a8
Hadi <hadi@hadis-macbook-pro.local>
2015-11-01 16:40:14 +0500

    Initial Commit

---
+++ Compiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Compiler
+//
+//  Created by Hadi on 01/11/2015.
+//  Copyright © 2015 Hadi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
